5254b7842e9873fba569711ea04ddbeccd2fdc52
Shine Suri <139244550+shineeeeeeeeeeee@users.noreply.github.com>
2026-02-06 11:28:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 def add(a, b):
-  a + b
+    return a + b
 
 def sub(a, b):
-  a - b
+    return a - b
 
 def mul(a, b):
-  a * b
+    return a * b
 
 def divide(a, b):
-  if b == 0:
-    return "Cannot divide by zero."
-  return a / b
+    if b == 0:
+        return "Cannot divide by zero."
+    return a / b
 
 if __name__ == "__main__":
-  print("Calculator App")
-  print(f"10 + 5 = {add(10, 5)}")
-  print(f"10 - 5 = {sub(10, 5)}")
-  print(f"10 * 5 = {mul(10, 5)}")
-  print(f"10 / 5 = {divide(10, 5)}")
-    
+    print("Calculator App")
+    print(f"10 + 5 = {add(10, 5)}")
+    print(f"10 - 5 = {sub(10, 5)}")
+    print(f"10 * 5 = {mul(10, 5)}")
+    print(f"10 / 5 = {divide(10, 5)}")
