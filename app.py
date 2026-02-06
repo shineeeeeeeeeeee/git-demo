3c7066fbf133959a1c737dcace221f60c20e29c6
Shine Suri <139244550+shineeeeeeeeeeee@users.noreply.github.com>
2026-02-06 11:18:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def divide(a, b):
     return "Cannot divide by zero."
   return a / b
 
-if __name__ = "__main__":
+if __name__ == "__main__":
   print("Calculator App")
   print(f"10 + 5 = {add(10, 5)}")
   print(f"10 - 5 = {sub(10, 5)}")
